9544ee84c4bbf1375dec490a256cd44aca0119e4
Jack Linton <jacklinton@mac.com>
2017-04-10 16:03:25 -0400

    Completely schema for posts and authors.

--- src/schema.js
+++ src/schema.js
@@ -1,21 +1,21 @@
-// import * as _ from 'underscore';
+import * as _ from 'underscore';
 
 // This is the Dataset in our blog
-// import PostsList from './data/posts';
-// import AuthorsList from './data/authors';
-// import {CommentList, ReplyList} from './data/comments';
+import PostsList from './data/posts';
+import AuthorsList from './data/authors';
+import {CommentList, ReplyList} from './data/comments';
 
 import {
   // These are the basic GraphQL types
-  // GraphQLInt,
-  // GraphQLFloat,
+  GraphQLInt,
+  GraphQLFloat,
   GraphQLString,
-  // GraphQLList,
+  GraphQLList,
   GraphQLObjectType,
-  // GraphQLEnumType,
+  GraphQLEnumType,
 
   // This is used to create required fields and arguments
-  // GraphQLNonNull,
+  GraphQLNonNull,
 
   // This is the class we need to create the schema
   GraphQLSchema
@@ -25,19 +25,55 @@ import {
   DEFINE YOUR TYPES BELOW
 **/
 
+const Post = new GraphQLObjectType({
+  name: "Post",
+  description: "This represent a Post",
+  fields: () => ({
+    _id: {type: new GraphQLNonNull(GraphQLString)},
+    title: {
+      type: new GraphQLNonNull(GraphQLString),
+      resolve: function(post) {
+        return post.title || "Does not exist";
+      }
+    },
+    content: {type: GraphQLString},
+    author: {
+      type: Author,
+      resolve: function(post) {
+        return _.find(AuthorsList, a => a._id == post.author);
+      }
+    }
+  })
+});
+
+const Author = new GraphQLObjectType({
+  name: "Author",
+  description: "This represent an author",
+  fields: () => ({
+    _id: {type: new GraphQLNonNull(GraphQLString)},
+    name: {type: GraphQLString}
+  })
+});
+
 // This is the Root Query
 const Query = new GraphQLObjectType({
   name: 'BlogSchema',
   description: 'Root of the Blog Schema',
   fields: () => ({
+    posts: {
+      type: new GraphQLList(Post),
+      resolve: function() {
+        return PostsList;
+      }
+    },
     echo: {
       type: GraphQLString,
       description: 'Echo what you enter',
       args: {
-        message: {type: GraphQLString}
+        message: {type: GraphQLString, description: "Give me a message"}
       },
       resolve: function(source, {message}) {
-        return `received ${message}`;
+        return {aa: 10};
       }
     }
   })
