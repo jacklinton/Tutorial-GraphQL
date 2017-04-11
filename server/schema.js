76dd5af25ec28d52061a4b80460c7e5113f07f02
Jack Linton <jacklinton@mac.com>
2017-04-10 23:33:54 -0400

    Finished the project but apparently had to downgrade to node version 4 for it to work. Can't imagine why that would be. Skipping that and considering everything complete.

--- server/schema.js
+++ server/schema.js
@@ -1,3 +1,7 @@
+const mongo = require('promised-mongo');
+// You can use any MONGO_URL here, whether it's locally or on cloud.
+const db = mongo('mongodb://localhost:27017/data/db');
+const authorsCollection = db.collection('authors');
 import * as _ from 'underscore';
 
 import {
@@ -15,7 +19,7 @@ const Author = new GraphQLObjectType({
   name: 'Author',
   description: 'Represent the type of an author of a blog post or a comment',
   fields: () => ({
-    _id: {type: GraphQLString},
+    _id: {type: GraphQLInt},
     name: {type: GraphQLString},
     twitterHandle: {type: GraphQLString}
   })
@@ -27,24 +31,26 @@ const Query = new GraphQLObjectType({
     authors: {
       type: new GraphQLList(Author),
       resolve: function() {
-        return [];
+        return authorsCollection.find().toArray();
       }
     }
   }
 });
 
 const Mutation = new GraphQLObjectType({
-  name: 'Mutations',
+  name: "Mutations",
   fields: {
     createAuthor: {
       type: Author,
       args: {
-        _id: {type: new GraphQLNonNull(GraphQLString)},
+        _id: {type: new GraphQLNonNull(GraphQLInt)},
         name: {type: new GraphQLNonNull(GraphQLString)},
         twitterHandle: {type: GraphQLString}
       },
       resolve: function(rootValue, args) {
-        throw new Error('Not Implemented')
+        let author = Object.assign({}, args);
+        return authorsCollection.insert(author)
+          .then(_ => author);
       }
     }
   }
